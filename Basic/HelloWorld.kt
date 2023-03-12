46ad88bd39acb5b2e069a388502f0750b0062b70
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-03-12 19:56:00 +0530

    data typr in kotlin

--- Basic/HelloWorld.kt
+++ Basic/HelloWorld.kt
@@ -1,3 +1,7 @@
+/*
+    @Data type in kotlin
+*/
+
 fun main() {
     println("Hello World")
 
@@ -12,15 +16,71 @@ fun main() {
     // name1="rajkishor"    //   ->  error: val cannot be reassigned
     println(name1);
 
+    var str : String
+    str="raja"
+    println(str)
 
-} 
+    var myInteger : Int
+    myInteger=100000
+    println(myInteger)
+
+    var myByte : Byte
+    myByte = 127
+    println(myByte)
+
+    var myShort : Short
+    myShort = 100
+    println(myShort);
+
+    var myLong : Long
+
+    myLong = 7739249804L
+    println(myLong);
+
+    /*
+        Flooting Types
+        
+        1. Floot  Holds 32 Bits
+        2. Double Holds 32 Bits
+    */
+
+    val myFloat : Float
+    myFloat = 3122.143467F
+    println(myFloat)
+
+    val myDouble : Double
+    myDouble = 129873.34898966699
+    println(myDouble)
+
+    val myChar : Char
+    myChar='A'
+    println("char: $myChar");
+
+    val wannaCoffe : Boolean
+    wannaCoffe = false
+    println("wanna Coffe ?: $wannaCoffe")
+
+}
 
 /*  
     Output:
-
     Hello World
     Hi, Raj Kishor
     Raj Kishorraj
     raj
+    raja
+    100000
+    127
+    Hi, Raj Kishor
+    Raj Kishorraj
+    raj
+    raja
+    100000
+    127
+    1007739249804
+    3122.1436
+    129873.34898966699
+    char: A
+    wanna Coffe ?: false
 
 */
